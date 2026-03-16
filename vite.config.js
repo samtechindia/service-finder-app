867ef9367ee2aed34482aed83e794fab024eed6d
ShivomHostel <136901730+ShivomHostel@users.noreply.github.com>
2026-03-16 19:54:21 +0530

    fix vercel issue

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'public'
+    outDir: 'dist',
+    chunkSizeWarningLimit: 2000
   },
   publicDir: 'static'
 })
